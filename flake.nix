6b4d97b86fa6984f52e244b9c8a4ad4f987c2a4e
jiricmi <accounts@mjiricek.eu>
2025-02-25 18:16:23 +0100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      crossPkgs = pkgs.pkgsCross.arm-embedded;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          stlink
+          crossPkgs.buildPackages.gdb
+          gcc-arm-embedded
+          openocd
+          cmake
+          putty
+          stm32cubemx
+          clang-tools
+          glibc_multi
+        ];
+
+        shellHook = ''
+          export OPENOCD_PATH="${pkgs.openocd}"
+        '';
+      };
+    };
+}
